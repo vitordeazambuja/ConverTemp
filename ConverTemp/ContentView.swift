df926c371543d94ea6fd4c25b83b91c934aefa36
Vitor <vitordeazambuja@edu.uniube.br>
2024-02-29 19:15:46 -0300

    Initial Commit

---
+++ ConverTemp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConverTemp
+//
+//  Created by Vitor de Azambuja on 29/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ConverTemp/ConverTempApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverTempApp.swift
+//  ConverTemp
+//
+//  Created by Vitor de Azambuja on 29/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ConverTempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
