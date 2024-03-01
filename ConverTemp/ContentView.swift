cde5c84cc7ee04a40201d409e9cc5ab4259918cf
Vitor <vitordeazambuja@edu.uniube.br>
2024-03-01 11:24:35 -0300

    Finish project

--- ConverTemp/ContentView.swift
+++ ConverTemp/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  ConverTemp
-//
-//  Created by Vitor de Azambuja on 29/02/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
@@ -15,34 +8,82 @@ struct ContentView: View {
     
     let units = ["Celsius", "Fahrenheit", "Kelvin"]
     
+    var convertedUnit: Double {
+        var temperaturaFinal: Double
+        switch inputUnit {
+            
+        case "Celsius":
+            if outputUnit == "Fahrenheit" {
+                temperaturaFinal = (inputNumber * 9/5) + 32
+                return temperaturaFinal
+            } else if outputUnit == "Kelvin" {
+                temperaturaFinal = inputNumber + 273.15
+                return temperaturaFinal
+            } else {
+                return inputNumber
+            }
+            
+        case "Fahrenheit":
+            if outputUnit == "Celsius" {
+                temperaturaFinal = (inputNumber - 32) * 5/9
+                return temperaturaFinal
+            } else if outputUnit == "Kelvin" {
+                temperaturaFinal = (inputNumber-32) * 5/9 + 273.15
+                return temperaturaFinal
+            } else {
+                return inputNumber
+            }
+            
+        case "Kelvin":
+            if outputUnit == "Celsius" {
+                temperaturaFinal = inputNumber - 273.15
+                return temperaturaFinal
+            } else if outputUnit == "Fahrenheit" {
+                temperaturaFinal = 1.8 * (inputNumber - 273) + 32
+                return temperaturaFinal
+            } else {
+                return inputNumber
+            }
+            
+        default:
+            return 0
+        }
+    }
     
     var body: some View {
-        NavigationStack{
-            Form{
+        NavigationView {
+            Form {
                 // Input
-                Section("Enter a number and a unit"){
+                Section("Enter a number and a unit") {
                     TextField("Input Number", value: $inputNumber, format: .number)
                         .keyboardType(.decimalPad)
-                    Picker("Input Unit", selection: $inputUnit){
-                        ForEach(units, id: \.self){
+                    Picker("Input Unit", selection: $inputUnit) {
+                        ForEach(units, id: \.self) {
                             Text("\($0)")
                         }
-                    }.pickerStyle(.segmented)
+                    }
+                    .pickerStyle(.segmented)
                 }
                 
                 // Output
-                Section("Select the output unit"){
-                    Picker("Input Unit", selection: $outputUnit){
-                        ForEach(units, id: \.self){
+                Section("Select the output unit") {
+                    Picker("Output Unit", selection: $outputUnit) {
+                        ForEach(units, id: \.self) {
                             Text("\($0)")
                         }
-                    }.pickerStyle(.segmented)
+                    }
+                    .pickerStyle(.segmented)
+                    Text(String(format: "%.2f", convertedUnit) + " \(outputUnit)")
+
                 }
-            }.navigationTitle("ConverTemp")
+            }
+            .navigationTitle("ConverTemp")
         }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
